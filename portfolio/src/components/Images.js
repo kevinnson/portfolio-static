1bdbf5115df91ccba4ff75e02c1cf42fdc18706e
Kevin Son <kevinson@umail.ucsb.edu>
2020-04-25 18:21:10 -0700

    react set up with nav bar

---
+++ portfolio/src/components/Images.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Images extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the Images page</h1>
+            </div>
+        );
+    }
+}
+
+export default Images;
